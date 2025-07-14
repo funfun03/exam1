cdee72a2809d520d852e4fec9e00995f1290ac97
Phuong <phuong9agt@gmail.com>
2025-07-14 10:03:54 +0700

    feat: Add Navbar component and integrate routing for User Management System

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import "./App.css";
 import UserForm from "./components/UserForm";
 import UserList from "./components/UserList";
+import UserManagementSystem from "./components/UserManagementSystem";
 import { UserProvider } from "./components/UserProvider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <UserProvider>
-      <div className="min-h-screen bg-gray-100 py-8">
-        <div className="container mx-auto px-4">
-          <h1 className="text-3xl font-bold text-center mb-8">
-            User Management System
-          </h1>
-          <UserForm />
-          <UserList />
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UserManagementSystem />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/users/:id" element={<UserManagementSystem />} />
+        </Routes>
+      </BrowserRouter>
     </UserProvider>
   );
 }
---
+++ src/components/UserManagementSystem.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import UserForm from "./UserForm";
+import UserList from "./UserList";
+import Navbar from "./Navbar";
+
+const UserManagementSystem = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="min-h-screen bg-gray-100 py-8">
+        <div className="container mx-auto px-4">
+          <h1 className="text-3xl font-bold text-center mb-8">
+            User Management System
+          </h1>
+          <UserForm />
+          <UserList />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserManagementSystem;
